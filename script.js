a31f5466bc3dd41de15873ba4031699b0ec1c870
HartleyE1 <67687015+HartleyE1@users.noreply.github.com>
2020-07-02 22:26:45 +1000

    nuff said

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var fs = require('fs');
+var files = fs.readdirSync('/images/');
+
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<path + img>';
+    document.getElementById('image').src = imgStr;
+}
 
